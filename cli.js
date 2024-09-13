41c455f1850eb41c76eb283ba2bec646ae432cd5
Sadaf Al Arabi <35129264+Sadaaf@users.noreply.github.com>
2024-09-13 15:21:30 -0400

    feat: add cli command functionality

--- cli.js
+++ cli.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+const { argv } = require("node:process");
+
+const taskName = "";
+const taskStatus = "";
 
 /**
  * File path for the task file.
@@ -22,6 +26,44 @@ if (!fs.existsSync(filePath)) {
 let data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
 /**
+ * Read CLI arguments
+ *
+ * @returns {object} object containing the arguments
+ */
+const readCliArguments = () => {
+  const command = argv[2];
+  switch (command) {
+    case "add":
+      createTask(argv[3]);
+      break;
+    case "update":
+      updateTaskName(argv[3], argv[4]);
+      break;
+    case "delete":
+      deleteTask(argv[3]);
+      break;
+    case "mark-in-progress":
+      updateTaskStatus(argv[3], "in-progress");
+      break;
+    case "mark-done":
+      updateTaskStatus(argv[3], "done");
+      break;
+    case "list":
+      if (!argv[3]) {
+        listAllTask();
+      } else {
+        listStatus(argv[3]);
+      }
+      break;
+    case "listOneTask":
+      listTask(argv[3]);
+      break;
+    default:
+      console.log("Invalid argument");
+  }
+};
+
+/**
  * Creates a new task.
  *
  * @param {string} taskName - Task Name
@@ -31,6 +73,7 @@ const createTask = (taskName) => {
   const taskData = {
     taskName: taskName,
     taskStatus: "todo",
+    // TODO Add createdAt and updatedAt
   };
   data[taskId] = taskData;
   console.log(`New task added with taskId ${taskId}`);
@@ -44,6 +87,7 @@ const createTask = (taskName) => {
  */
 const updateTaskName = (taskId, taskName) => {
   data[taskId]["taskName"] = taskName;
+  // TODO Update updatedAt
   console.log(`The task name of task with id ${taskId} is updated`);
 };
 
@@ -54,7 +98,9 @@ const updateTaskName = (taskId, taskName) => {
  * @param {string} taskStatus  - Task Status
  */
 const updateTaskStatus = (taskId, taskStatus) => {
-  data[taskId]["taskStatus"] = taskStatus;
+  console.log(taskId);
+  data[taskId].taskStatus = taskStatus;
+  // TODO UpdateUpdatedAt
   console.log(`The task status of task with id ${taskId} is updated`);
 };
 
@@ -72,7 +118,7 @@ const deleteTask = (taskId) => {
  * Shows all created tasks in the console
  */
 const listAllTask = () => {
-  console.log(data);
+  console.log(JSON.stringify(data));
 };
 
 /**
@@ -90,6 +136,18 @@ const listTask = (taskId) => {
  * @param {string} status
  */
 const listStatus = (status) => {
-  const newData = Object.keys(data).map((key) => data[key].status == status);
+  const newData = {};
+  Object.keys(data).map((key) => {
+    if (data[key].taskStatus === status) {
+      newData[key] = data[key];
+    }
+  });
   console.log(newData);
 };
+
+readCliArguments();
+
+/**
+ * Saving the data to json
+ */
+fs.writeFileSync(filePath, JSON.stringify(data));
